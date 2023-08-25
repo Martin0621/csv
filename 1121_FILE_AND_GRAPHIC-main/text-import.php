ef33511edd0895f0854b90894b494f2175104f01
Martin <cheyuan0621@icloud.com>
2023-08-25 09:18:36 +0800

    csv檔案上傳網頁

---
+++ 1121_FILE_AND_GRAPHIC-main/text-import.php
@@ -0,0 +1,69 @@
+<style>
+
+table{
+    border:1px solid #ccc;
+    box-shadow: 3px 3px 45px #aaa;
+    padding:20px;
+    border-collapse: collapse;
+}    
+
+td{
+    border:1px solid #ccc;
+    padding:5px 10px;
+    text-align: center;
+}
+</style>
+<?php
+/****
+ * 1.建立資料庫及資料表
+ * 2.建立上傳檔案機制
+ * 3.取得檔案資源
+ * 4.取得檔案內容
+ * 5.建立SQL語法
+ * 6.寫入資料庫
+ * 7.結束檔案
+ */
+
+
+if(!empty($_FILES['text']['tmp_name'])){
+    echo "檔名是:".$_FILES['text']['name'];
+    echo "<br>";
+    echo "檔案大小是:".$_FILES['text']['size'];
+    echo "<hr>";
+
+    move_uploaded_file($_FILES['text']['tmp_name'],"./document/{$_FILES['text']['name']}");
+    $path="./document/{$_FILES['text']['name']}";
+    // 使用 'r' 模式來讀取檔案
+$file = fopen($path, "r");
+echo "<table>";
+if ($file) {
+    // 讀取檔案到最後
+    while (($line = fgets($file)) !== false) {
+        echo "<tr>";
+        $cols=explode(",",$line);
+        for($i=0;$i<count($cols);$i++){
+            echo "<td>";
+            echo $cols[$i];
+            echo "</td>";
+        }
+        
+        echo "</tr>";
+    }
+    fclose($file);
+} else {
+    // 檔案開啟失敗
+    echo "檔案開啟失敗";
+}
+
+echo "</table>";
+}
+
+?>
+
+
+<!----讀出匯入完成的資料----->
+
+
+
+</body>
+</html>
